7838389b136cb2c0fc9890cd1e25b02d951adaec
Bertrand Néron <freeh4cker@gmail.com>
2017-05-07 01:02:49 +0200

    initial commit to publish pages

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to The Pythonic Way&#8217;s web site!"],titleterms:{The:0,document:[],indic:0,python:0,site:0,tabl:0,wai:0,web:0,welcom:0}})
